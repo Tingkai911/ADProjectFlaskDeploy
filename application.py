b56613b4007202455f4d90dc0c84af6924e7e3f3
Tingkai911 <tingkai911@gmail.com>
2021-02-18 10:58:27 +0800

    help

--- application.py
+++ application.py
@@ -20,6 +20,7 @@ import pyodbc
 
 from urllib import parse
 
+
 application = Flask(__name__)
 application.secret_key="password123"
 
@@ -102,7 +103,7 @@ def generate_allergen_tage():
     return jsonify(return_data)
 
 def getDataFromDB():
-    params = parse.quote_plus("Driver={unixODBC};Server=tcp:recipe4medbserver.database.windows.net,1433;Database=Recipe4Me_db;Uid=recipe4me;Pwd={password@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    params = parse.quote_plus("Driver={ODBC Driver 17 for SQL Server};Server=tcp:recipe4medbserver.database.windows.net,1433;Database=Recipe4Me_db;Uid=recipe4me;Pwd={password@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
     engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
     tag_name = []
